abbbe184aab339ae956acb248b4d82902617754d
Renato Ferreira <renatoredes@live.com>
2019-08-04 02:15:14 -0300

    Em desenvolvimento

---
+++ src/main/java/com/br/api/api/config/WebSecurityConfigurerAdapterCustom.java
@@ -0,0 +1,47 @@
+package com.br.api.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableResourceServer
+@Configuration
+public class WebSecurityConfigurerAdapterCustom extends WebSecurityConfigurerAdapter {
+	 @Autowired
+	    private BCryptPasswordEncoder passwordEncoder;
+
+	    @Autowired
+	    public void globalUserDetails(final AuthenticationManagerBuilder auth) throws Exception {
+	        // @formatter:off
+	        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("admin")).roles("USER");
+	    }
+
+	    @Override
+	    @Bean
+	    public AuthenticationManager authenticationManagerBean() throws Exception {
+	        return super.authenticationManagerBean();
+	    }
+
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and()
+	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+
+	    }
+
+	    @Override
+	    public void configure(WebSecurity web) throws Exception {
+	        web.ignoring().antMatchers("/categorias");
+	    }
+
+	}

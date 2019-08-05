b95f4dfe3676abe058e4e0bc3948e7c77f14a42b
Renato Melo <renatoredes@live.com>
2019-08-05 09:55:00 -0300

    documentation

---
+++ wiki/ExemplosClasse/SecurityConfigExemploBasic.java
@@ -0,0 +1,33 @@
+/*
+package com.br.api.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigExemploBasic extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			
+		auth.inMemoryAuthentication().withUser("admin").roles("ADMIN").password("{noop}admin");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+				 http.authorizeRequests()
+				.antMatchers("/categorias").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic().and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.csrf().disable();
+	}
+	
+}
+*/

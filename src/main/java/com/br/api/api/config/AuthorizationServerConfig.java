741fd71e598a53c2d475ecf9877a3fe3bc456e06
Renato Melo <renatoredes@live.com>
2019-08-05 12:40:18 -0300

    develope atualizada

--- src/main/java/com/br/api/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/br/api/api/config/AuthorizationServerConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
+/**/
 
 @Configuration
 @EnableAuthorizationServer

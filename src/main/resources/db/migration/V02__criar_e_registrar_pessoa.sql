87b40cf6273779288c72ea1cb0c88f45c6aeee57
Renato Ferreira <renatoredes@live.com>
2019-08-16 22:39:59 -0300

    ok

--- src/main/resources/db/migration/V02__criar_e_registrar_pessoa.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoa.sql
@@ -1,18 +1,3 @@
-DROP TABLE IF EXISTS pessoa;
-
-CREATE TABLE pessoa (
-	codigo SERIAL PRIMARY KEY,
-	nome VARCHAR(50) NOT NULL,
-	logradouro VARCHAR(30),
-	numero VARCHAR(30),
-	complemento VARCHAR(30),
-	bairro VARCHAR(30),
-	cep VARCHAR(30),
-	cidade VARCHAR(30),
-	estado VARCHAR(30),
-	ativo BOOLEAN NOT NULL
-) ;
-
 
 INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('João Silva', 'Rua do Abacaxi', '10', null, 'Brasil', '38.400-12', 'Uberlândia', 'MG', true);
 INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Maria Rita', 'Rua do Sabiá', '110', 'Apto 101', 'Colina', '11.400-12', 'Ribeirão Preto', 'SP', true);

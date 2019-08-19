521127a51df1ab108c7a6ddeed0e4b59964379c0
Renato Melo <renatoredes@live.com>
2019-08-19 10:52:15 -0300

    Procfile, Depedencias do PostgreSQL e Procfile

--- src/main/resources/db/migration/V03__criar_tabela_e_registrar_lancamentos.sql
+++ src/main/resources/db/migration/V03__criar_tabela_e_registrar_lancamentos.sql
@@ -1,16 +1,16 @@
-CREATE TABLE lancamento (
+CREATE TABLE IF NOT EXISTS lancamento (
 	codigo SERIAL PRIMARY KEY,
 	descricao VARCHAR(50) NOT NULL,
 	data_vencimento DATE NOT NULL,
 	data_pagamento DATE,
-	valor DECIMAL(10,2) NOT NULL,
+	valor numeric (10,2) NOT NULL,
 	observacao VARCHAR(100),
 	tipo VARCHAR(20) NOT NULL,
-	codigo_categoria BIGINT(20) NOT NULL,
-	codigo_pessoa BIGINT(20) NOT NULL,
+	codigo_categoria integer NOT NULL,
+	codigo_pessoa integer NOT NULL,
 	FOREIGN KEY (codigo_categoria) REFERENCES categoria(codigo),
 	FOREIGN KEY (codigo_pessoa) REFERENCES pessoa(codigo)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Salário mensal', '2017-06-10', null, 6500.00, 'Distribuição de lucros', 'RECEITA', 1, 1);
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Bahamas', '2017-02-10', '2017-02-10', 100.32, null, 'DESPESA', 2, 2);
--- src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -1,25 +1,25 @@
 CREATE TABLE usuario (
-	codigo SERIAL PRIMARY KEY,
+	codigo serial PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	senha VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE permissao (
-	codigo int PRIMARY KEY,
+	codigo serial PRIMARY KEY,
 	descricao VARCHAR(50) NOT NULL
-);
+) ;
 
 CREATE TABLE usuario_permissao (
-	codigo_usuario int NOT NULL,
-	codigo_permissao int NOT NULL,
+	codigo_usuario integer NOT NULL,
+	codigo_permissao integer NOT NULL,
 	PRIMARY KEY (codigo_usuario, codigo_permissao),
 	FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo),
 	FOREIGN KEY (codigo_permissao) REFERENCES permissao(codigo)
 ) ;
 
-INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@apialfa.com', '$2a$10$VrBYyXl7rxonT7OSCVy7VOx.t27X3OvjFF0n0fjLeMvgujwJWXnZ2');
-INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Lucas Ferreira', 'lucas@apialfa.com', '$2a$10$t1/HfqCCCHk./bV.NGz4Fu2hIR3cCpBGhR.xe1pSLySo1rphy84xO');
+INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@algamoney.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Maria Silva', 'maria@algamoney.com', '$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
 
 INSERT INTO permissao (codigo, descricao) values (1, 'ROLE_CADASTRAR_CATEGORIA');
 INSERT INTO permissao (codigo, descricao) values (2, 'ROLE_PESQUISAR_CATEGORIA');

6e9ea85a01376a76b57faa92bdbd1bd5e314f545
Renato Melo <renatoredes@live.com>
2019-08-19 13:37:06 -0300

    WebSecurityConfigurerAdapterCustom

--- src/main/java/com/br/api/api/config/WebSecurityConfigurerAdapterCustom.java
+++ src/main/java/com/br/api/api/config/WebSecurityConfigurerAdapterCustom.java
@@ -1,48 +1,21 @@
 package com.br.api.api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableWebSecurity
 @EnableResourceServer
 @Configuration
 public class WebSecurityConfigurerAdapterCustom extends WebSecurityConfigurerAdapter {
-	
-	   
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	//o recurso categorias não é nescessario autenticação
-        http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
-
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/categorias");
-    }
-/*	
 	@Override
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	*/
-	
+
 }

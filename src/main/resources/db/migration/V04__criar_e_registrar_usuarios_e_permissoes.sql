3b63e39375a1cd4502786864543033699d64283b
Renato Ferreira <renatoredes@live.com>
2019-08-19 23:27:36 -0300

    config

--- src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -1,25 +1,25 @@
 CREATE TABLE usuario (
-	codigo BIGINT(20) PRIMARY KEY,
+	codigo serial PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	senha VARCHAR(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE permissao (
-	codigo BIGINT(20) PRIMARY KEY,
+	codigo serial PRIMARY KEY,
 	descricao VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ;
 
 CREATE TABLE usuario_permissao (
-	codigo_usuario BIGINT(20) NOT NULL,
-	codigo_permissao BIGINT(20) NOT NULL,
+	codigo_usuario integer NOT NULL,
+	codigo_permissao integer NOT NULL,
 	PRIMARY KEY (codigo_usuario, codigo_permissao),
 	FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo),
 	FOREIGN KEY (codigo_permissao) REFERENCES permissao(codigo)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ;
 
-INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@apialfa.com', '$2a$10$VrBYyXl7rxonT7OSCVy7VOx.t27X3OvjFF0n0fjLeMvgujwJWXnZ2');
-INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Lucas Ferreira', 'lucas@apialfa.com', '$2a$10$t1/HfqCCCHk./bV.NGz4Fu2hIR3cCpBGhR.xe1pSLySo1rphy84xO');
+INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@apialfa.com', '$2a$10$xHFP3pL19P3nfW7jVr7toujcACXn3iEYgGUoB08guPgHCADoffRoW');
+INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Lucas', 'maria@apialfa.com', '$2a$10$2ll10NgFbPgKMxkJ6k0xd.RksWbvoUGua4PxQGVJ4Pt/5Tkx8sz7y');
 
 INSERT INTO permissao (codigo, descricao) values (1, 'ROLE_CADASTRAR_CATEGORIA');
 INSERT INTO permissao (codigo, descricao) values (2, 'ROLE_PESQUISAR_CATEGORIA');
@@ -42,7 +42,7 @@ INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 6);
 INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 7);
 INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 8);
 
--- lucas
+-- maria
 INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (2, 2);
 INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (2, 5);
 INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (2, 8);
